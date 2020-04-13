277e37df2524e7be43f08225fa8a8e613dc8f091
Fabio Calazans <fcalazans@fabiocalazans.com>
2020-04-13 14:39:19 +0100

    More functions, reading from data folder, better aproach and organiazation of the files.

--- includes/functions.php
+++ includes/functions.php
@@ -4,20 +4,64 @@
     function makeForm($form) {
 
         $loginForm = '<div class="form">
-    <label for="fn">User</label>
-    <input placeholder="Username" type="text" name="username" id="user" value="username">
+    <label for="username">Username</label>
+    <input placeholder="Username" type="text" name="username" id="username" value="username">
 
-
-    <label for="pass">Pass</label>
+    <label for="pass">Password</label>
     <input placeholder="Password" type="password" name="favourite" id="fav" value="password">
 
-    <input class="admin" type="submit" name="submitdetails" value="login">
-    <!-- <input type="submit" name="submit" value="Admin" /> --> </div>';
+    <input class="admin-btn" type="submit" name="submitdetails" value="login">';
 
         if ($form === "loginForm") {
-            echo $loginForm;
+            return $loginForm;
         } else {
 
             return $form;
+        }
+    }
+
+    function displayPage($page) {
+        $about = '<h1>About</h1>
+<hr />
+<p>
+    The Department of Computer Science and Information Systems at Birkbeck traces
+    its roots back to 1946 when the College’s first computer was built in the
+    Computer Laboratory founded by Dr Andew Booth. The Department of Computer
+    Science was established in 1957 and information about these early years can be
+    viewed in our history. Today the Department continues as a leading centre of
+    computing expertise, specialising in information and knowledge management, web
+    and pervasive technologies, computational intelligence and information systems
+    development.
+</p>
+<hr />';
+
+        $contact = '<h1>Contact</h1>
+<hr />
+<p>Our address</p>
+<p>Department of Computer Science and Information Systems</p>
+<p>Birkbeck, University of London</p>
+<p>Malet Street</p>
+<p>London WC1E 7HX</p>
+<p>+44 (0)20 7631 6700</p>
+<p>Directions, transport, maps and accommodation</p>
+<hr />';
+
+        $services = '<h1>Services</h1>
+<hr />
+<p>Undergraduate</p>
+<p>Postgraduate</p>
+<p>Postgraduate Specialist</p>
+<p>Postgraduate Research</p>
+<p>Short Courses and Certificates</p>
+<p>Modules</p>
+<p>Fees</p>
+<hr />';
+
+        if ($page == "about") {
+            return $about;
+        } elseif ($page == "contact") {
+            return $contact;
+        } elseif ($page == "services") {
+            return $services;
     }
 }
--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<?php include 'includes/header.php';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='//fonts.googleapis.com/css?family=Roboto:300,400,700,400italic,700italic' rel='stylesheet'
+        type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+    <title>DCS | Home</title>
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <?php include 'includes/header.php';?>
+        <div class="main-frame">
+            <?php // Create a handle
+                $handle = opendir('data');
+
+                // Open directory and read contents.
+                while (false !== ($file = readdir($handle))) {
+
+                    // Check if file extension is valid to read
+                    if (is_file('data/' . $file)) {
+                        $path_extension = pathinfo($file);
+                        if ($path_extension['extension'] != 'html') {
+                            echo "<p> data $file : INVALID FILE EXTENSION- should be .html</p>";
+                        }
+                        if ($path_extension['extension'] == 'html') {
+                            $fileNames[] = pathinfo($file, PATHINFO_FILENAME);
+                            $files[] = $file;
+                        }
+                    }
+                }
+                // Close handle.
+                closedir($handle);
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
